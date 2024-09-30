3c0057b5cf8965c57eca38b43b30b509ffc05e45
TSAQIF AHMAD <tsaqifahmad12@student.ub.ac.id>
2024-09-30 12:27:17 +0700

    commit

--- losikbank/uang/urls.py
+++ losikbank/uang/urls.py
@@ -15,5 +15,15 @@ urlpatterns = [
     path('read_jenis_pekerjaan', views.read_jenis_pekerjaan, name='read_jenis_pekerjaan'),
     path('update_jenis_pekerjaan/<str:id>', views.update_jenis_pekerjaan, name='update_jenis_pekerjaan'),
     path('delete_jenis_pekerjaan/<str:id>', views.delete_jenis_pekerjaan, name='delete_jenis_pekerjaan'),
+    #CRUD Nasabah
+    path('create_nasabah', views.create_nasabah, name='create_nasabah'),
+    path('read_nasabah', views.read_nasabah, name='read_nasabah'),
+    path('update_nasabah/<str:id>', views.update_nasabah, name='update_nasabah'),
+    path('delete_nasabah/<str:id>', views.delete_nasabah, name='delete_nasabah'),
+    #CRUD Limit Peminjaman
+    path('create_limit_peminjaman', views.create_limit_peminjaman, name='create_limit_peminjaman'),
+    path('read_limit_peminjaman', views.read_limit_peminjaman, name='read_limit_peminjaman'),
+    path('update_limit_peminjaman/<str:id>', views.update_limit_peminjaman, name='update_limit_peminjaman'),
+    path('delete_limit_peminjaman/<str:id>', views.delete_limit_peminjaman, name='delete_limit_peminjaman'),
 ]
 
